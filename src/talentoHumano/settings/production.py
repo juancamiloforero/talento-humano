aa777957af6e8d8604f55605cc954adeec186195
Camilo Forero <juancamiforero@unicauca.edu.co>
2020-04-30 17:32:23 -0500

    Recuperar contraseña terminado

--- src/talentoHumano/settings/production.py
+++ src/talentoHumano/settings/production.py
@@ -17,3 +17,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
--- src/talentoHumano/urls.py
+++ src/talentoHumano/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('after_login', after_login, name='afterLogin'),
     path('logout', auth_views.logout_then_login, name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset_form.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
 ]
